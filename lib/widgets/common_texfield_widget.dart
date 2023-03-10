0869df89494478c0bab4d0b9ce015f92c0e1d9cd
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-09 21:37:44 -0500

    Mods

--- lib/widgets/common_texfield_widget.dart
+++ lib/widgets/common_texfield_widget.dart
@@ -1,5 +1,6 @@
 import 'package:codigo6_alertas/utils/types.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CommonTextFieldWidget extends StatelessWidget {
   String label;
@@ -37,7 +38,16 @@ class CommonTextFieldWidget extends StatelessWidget {
             keyboardType: type == InputType.dni || type == InputType.phone
                 ? TextInputType.number
                 : TextInputType.text,
+            maxLength: type == InputType.dni
+                ? 8
+                : type == InputType.phone
+                    ? 9
+                    : null,
+            inputFormatters: type == InputType.dni || type == InputType.phone
+                ? [FilteringTextInputFormatter.allow(RegExp(r"[0-9]"))]
+                : [],
             decoration: InputDecoration(
+              counterText: "",
               hintText: hintText,
               hintStyle: TextStyle(
                 fontSize: 14.0,
