bd2eb5d6949f816997a48fb5696cb897ecb78532
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-13 22:54:45 -0500

    Mods

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,3 +1,5 @@
+import 'package:codigo6_alertas/services/api_service.dart';
+import 'package:codigo6_alertas/widgets/general_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -8,23 +10,32 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   Set<Marker> myMarkers = {};
+  ApiService apiService = ApiService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(-16.373062, -71.528785),
-          zoom: 14,
-        ),
-        markers: myMarkers,
-        onTap: (LatLng position) {
-          Marker marker = Marker(
-            markerId: MarkerId(myMarkers.length.toString()),
-            position: position,
-          );
-          myMarkers.add(marker);
-          setState(() {});
+      body: FutureBuilder(
+        future: apiService.getIncidents(),
+        builder: (BuildContext context, AsyncSnapshot snap) {
+          if (snap.hasData) {
+            return GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(-16.373062, -71.528785),
+                zoom: 8,
+              ),
+              markers: myMarkers,
+              onTap: (LatLng position) {
+                Marker marker = Marker(
+                  markerId: MarkerId(myMarkers.length.toString()),
+                  position: position,
+                );
+                myMarkers.add(marker);
+                setState(() {});
+              },
+            );
+          }
+          return loadingWidget;
         },
       ),
     );
