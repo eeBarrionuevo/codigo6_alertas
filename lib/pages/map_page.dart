bec874637de9ac1c1deb3c85df9a935052d758ea
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-13 23:10:14 -0500

    Mods

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,3 +1,4 @@
+import 'package:codigo6_alertas/models/incident_model.dart';
 import 'package:codigo6_alertas/services/api_service.dart';
 import 'package:codigo6_alertas/widgets/general_widget.dart';
 import 'package:flutter/material.dart';
@@ -19,10 +20,20 @@ class _MapPageState extends State<MapPage> {
         future: apiService.getIncidents(),
         builder: (BuildContext context, AsyncSnapshot snap) {
           if (snap.hasData) {
+            List<IncidentModel> incidents = snap.data;
+
+            incidents.forEach((item) {
+              Marker myMarker = Marker(
+                markerId: MarkerId(myMarkers.length.toString()),
+                position: LatLng(item.latitud, item.longitud),
+              );
+              myMarkers.add(myMarker);
+            });
+
             return GoogleMap(
               initialCameraPosition: CameraPosition(
                 target: LatLng(-16.373062, -71.528785),
-                zoom: 8,
+                zoom: 5,
               ),
               markers: myMarkers,
               onTap: (LatLng position) {
