d74c117d77064289b608be5e9c121a7adc4fa249
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-16 20:42:48 -0500

    Mods

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,6 +1,11 @@
+import 'package:codigo6_alertas/models/news_model.dart';
+import 'package:codigo6_alertas/services/api_service.dart';
+import 'package:codigo6_alertas/widgets/general_widget.dart';
 import 'package:flutter/material.dart';
 
 class NewsPage extends StatelessWidget {
+  ApiService apiService = ApiService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +24,27 @@ class NewsPage extends StatelessWidget {
                 ),
                 const SizedBox(
                   height: 12.0,
+                ),
+                FutureBuilder(
+                  future: apiService.getNews(),
+                  builder: (BuildContext context, AsyncSnapshot snap) {
+                    if (snap.hasData) {
+                      List<NewsModel> news = snap.data;
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        physics: const BouncingScrollPhysics(),
+                        itemCount: news.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Row(
+                            children: [
+                              Image.network(news[index].imagen),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                    return loadingWidget;
+                  },
                 ),
               ],
             ),
