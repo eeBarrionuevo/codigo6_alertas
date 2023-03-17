9a21881b6b944d52aedf4e29d506a9c5205ae6fe
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-03-16 22:03:52 -0500

    Mods

--- lib/pages/news_form_page.dart
+++ lib/pages/news_form_page.dart
@@ -1,8 +1,78 @@
+import 'package:codigo6_alertas/utils/types.dart';
+import 'package:codigo6_alertas/widgets/common_button_widget.dart';
+import 'package:codigo6_alertas/widgets/common_texfield_widget.dart';
 import 'package:flutter/material.dart';
 
 class NewsFormPage extends StatelessWidget {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _linkController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      "Registrar Noticia",
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    CommonTextFieldWidget(
+                      label: "Título",
+                      hintText: "Ingresa un título",
+                      type: InputType.text,
+                      controller: _titleController,
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    CommonTextFieldWidget(
+                      label: "Link",
+                      hintText: "Ingresa un link",
+                      type: InputType.text,
+                      controller: _linkController,
+                    ),
+                    Row(
+                      children: [
+                        ElevatedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.image),
+                          label: Text("Galería"),
+                        ),
+                        ElevatedButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.camera),
+                          label: Text("Cámara"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: CommonButtonWidget(
+                text: "Registrar",
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
